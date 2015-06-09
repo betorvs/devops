66b1ffd70c00d538d09179460611884d5c02565d
betorvs <beto.rvs@gmail.com>
2015-06-09 18:47:00 -0300

    scripts

--- resize.pp
+++ resize.pp
@@ -4,26 +4,26 @@ $partition = 'opt'
 $vg = 'oi01'
 
 exec { 'umount':
-  command => "/bin/umount /${particion}",
-  onlyif => ["/bin/mount |/bin/grep -q opt","/sbin/lvdisplay /dev/mapper/${vg}-${particion} |grep -q '5..... GiB'" ]
+  command => "/bin/umount /${partition}",
+  onlyif => ["/bin/mount |/bin/grep -q opt","/sbin/lvdisplay /dev/mapper/${vg}-${partition} |grep -q '8..... GiB'" ]
 }
 exec { 'lvremove':
-  command => "/sbin/lvremove /dev/mapper/${vg}-${particion}",
-  unless => "/sbin/lvdisplay |grep -q ${particion}",
+  command => "/sbin/lvremove --force /dev/mapper/${vg}-${partition}",
+  unless => "/sbin/lvdisplay /dev/mapper/${vg}-${partition}|grep -q '4.00 GiB'",
   require => Exec["umount"],
 }
 exec { 'lvcreate':
-  command => "/sbin/lvcreate -n ${particion} --size 4093MB ${vg}",
-  unless => "/sbin/lvdisplay /dev/mapper/${vg}-${particion}|grep -q '4.00 GiB'",
+  command => "/sbin/lvcreate -n ${partition} --size 4093MB ${vg}",
+  unless => "/sbin/lvdisplay /dev/mapper/${vg}-${partition}|grep -q '4.00 GiB'",
   require => Exec["lvremove"],
 }
 exec { 'mkfs':
-  command => "/sbin/mkfs.ext4 /dev/mapper/${vg}-${particion}",
-  unless => "/sbin/lvdisplay |grep -q ${particion}",
+  command => "/sbin/mkfs.ext4 /dev/mapper/${vg}-${partition}",
+#  onlyif => "/bin/mount /${partition}", 
   require => Exec["lvcreate"],
 }
 exec { 'mount':
-  command => "/bin/mount /${particion}",
-  unless => "/bin/grep -q ${particion} /etc/fstab"
+  command => "/bin/mount /${partition}",
+  onlyif => ["/bin/grep -q ${partition} /etc/fstab", "/bin/mount /${partition}"],
   require => Exec["mkfs"],
 }
