73ec1ca34eaf78ec30eebb4de2274d8e16f71118
betorvs <beto.rvs@gmail.com>
2015-07-16 10:10:58 -0300

    add SO

--- docker-mkpkg/testrpm.sh
+++ docker-mkpkg/testrpm.sh
@@ -3,18 +3,18 @@
 DIR="$PWD/$1"
 
 case $2 in 
-  "centos6")
-    $SO="centos:centos6"
+  centos6)
+    SO="centos:centos6"
     ;;
-  "centos5")
-    $SO="centos:centos5"
+  centos5)
+    SO="centos:centos5"
     ;;
-   "debian")
-    $SO="debian:wheezy"
+  debian)
+    SO="debian:wheezy"
    ;;
-   *)
-    $SO="centos:centos6"
+  *)
+    SO="centos:centos6"
    ;;
 
 esac
-docker run --rm -t -v $DIR:/vol -i $SO /bin/bash
+echo "docker run --rm -t -v $DIR:/vol -i $SO /bin/bash"
