1c19d9faeefe0b7cb895318d7705dd11a69589f5
Roberto Scudeller <roberto.scudeller@walmart.com>
2016-11-03 12:17:43 -0200

    outro exemplo

--- grafana-dashboard/vm.js
+++ grafana-dashboard/vm.js
@@ -28,7 +28,7 @@ dashboard = {
 };
 
 // Set a title
-dashboard.title = 'grafana.oi.infra';
+dashboard.title = 'grafana 2 dashboard';
 
 // Set default time
 // time can be overriden in the url using from/to parameters, but this is
